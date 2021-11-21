7350cc3d88cb997de522e9cb983bea74a1f0731f
jefferson marin <jeffersondariom@gmail.com>
2021-11-21 18:00:17 -0500

    20. Uso de JPQL en anotación query

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import java.lang.reflect.Array;
 import java.time.LocalDate;
@@ -30,13 +31,13 @@ public class FundamentosApplication implements CommandLineRunner {
     private UserPojo userPojo;
     private UserRepository userRepository;
 
-    public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo,UserRepository userRepository) {
+    public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyBeanWithProperties myBeanWithProperties, UserPojo userPojo, UserRepository userRepository) {
         this.componentDependency = componentDependency;
         this.myBean = myBean;
         this.myBeanWithDependency = myBeanWithDependency;
         this.myBeanWithProperties = myBeanWithProperties;
         this.userPojo = userPojo;
-        this.userRepository=userRepository;
+        this.userRepository = userRepository;
     }
 
     public static void main(String[] args) {
@@ -45,25 +46,36 @@ public class FundamentosApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        ejemplosAnteriores();
+        //ejemplosAnteriores();
         saveUserInDataBase();
+        getInformationJpqlFromUser();
+    }
+
+    private void getInformationJpqlFromUser() {
+        LOGGER.info("Usuario con el metodo findByUserEmail " +
+                userRepository.findByUserEmail("Julie@domain.com").orElseThrow(() -> new RuntimeException("No se encontro el usuario")));
+        // Ordenar y bucar a partir de una propiedad
+        //ascending -> tambien muestra los usuarios de manera ascendente
+        userRepository.findAndSort("user", Sort.by("id").descending()).stream().forEach(user -> LOGGER.info("Usuario con metodo sort " + user));
+
+
     }
 
 
-    private void saveUserInDataBase(){
-        User user1 = new User("Jhon","Jhon@domain.com", LocalDate.of(2021,03,20));
-        User user2 = new User("Julie","Julie@domain.com", LocalDate.of(2021,05,21));
-        User user3 = new User("Daniela","Daniela@domain.com", LocalDate.of(2021,8,11));
-        User user4 = new User("user4","user4@domain.com", LocalDate.of(2021,7,12));
-        User user5 = new User("user5","user5@domain.com", LocalDate.of(2021,11,15));
-        User user6 = new User("user6","user6@domain.com", LocalDate.of(2021,2,9));
-        User user7 = new User("user7","user7@domain.com", LocalDate.of(2021,3,13));
-        User user8 = new User("user8","user8@domain.com", LocalDate.of(2021,4,16));
-        User user9 = new User("user9","user9@domain.com", LocalDate.of(2021,5,25));
-        User user10 = new User("user10","user10@domain.com", LocalDate.of(2021,8,7));
-        User user11 = new User("user11","user11@domain.com", LocalDate.of(2021,12,1));
-        User user12 = new User("user12","user12@domain.com", LocalDate.of(2021,1,15));
-        List<User> list = Arrays.asList(user1,user2,user3,user4,user5,user6,user7,user8,user9,user10,user11,user12);
+    private void saveUserInDataBase() {
+        User user1 = new User("Jhon", "Jhon@domain.com", LocalDate.of(2021, 03, 20));
+        User user2 = new User("Julie", "Julie@domain.com", LocalDate.of(2021, 05, 21));
+        User user3 = new User("Daniela", "Daniela@domain.com", LocalDate.of(2021, 8, 11));
+        User user4 = new User("user4", "user4@domain.com", LocalDate.of(2021, 7, 12));
+        User user5 = new User("user5", "user5@domain.com", LocalDate.of(2021, 11, 15));
+        User user6 = new User("user6", "user6@domain.com", LocalDate.of(2021, 2, 9));
+        User user7 = new User("user7", "user7@domain.com", LocalDate.of(2021, 3, 13));
+        User user8 = new User("user8", "user8@domain.com", LocalDate.of(2021, 4, 16));
+        User user9 = new User("user9", "user9@domain.com", LocalDate.of(2021, 5, 25));
+        User user10 = new User("user10", "user10@domain.com", LocalDate.of(2021, 8, 7));
+        User user11 = new User("user11", "user11@domain.com", LocalDate.of(2021, 12, 1));
+        User user12 = new User("user12", "user12@domain.com", LocalDate.of(2021, 1, 15));
+        List<User> list = Arrays.asList(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10, user11, user12);
         list.stream().forEach(userRepository::save);
     }
 
